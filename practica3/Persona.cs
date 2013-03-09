c5ff8eaf24bb948c9571d3099b584d1decd4aacc
Jonathan López <jona@localhost.localdomain>
2013-03-09 11:27:42 -0600

    Clase persona.

---
+++ practica3/Persona.cs
@@ -0,0 +1,21 @@
+using System;
+namespace practica3
+{
+	public class Persona
+	{
+		public string codigo;
+		public string nombre;
+		public string direccion;
+		public string facebook;
+		public string telefono;
+		
+		public Persona(){
+			this.codigo = "";
+			this.nombre = "";
+			this.direccion = "";
+			this.facebook = "";
+			this.telefono = "";
+		}
+	}
+}
+
